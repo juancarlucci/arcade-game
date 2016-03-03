af03e96fc83c86dfc8eb99819d72ad92e904b0dd
juancarlucci <juancarloscollins@gmail.com>
2016-03-03 14:29:58 -0800

    add player and enemy, not working

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
+// Speed in pixels per second. From jlongster on github
+var playerSpeed = 200;
+var enemySpeed = 100;
+
 // Enemies our player must avoid
+    //The constructor function takes in arguments for the aspects that
+    //will be different about each enemy. Here blank, ()
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = 10;
+    this.y = 20;
+    this.speed = enemySpeed;
+    // this.width = config.width || 30;
+    // this.height = config.height || 30;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +25,23 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    enemy.sprite.update(dt);
+    this.x = this.x + this.speed * dt;
+    //   if (this.x > ctx.width) {
+    // this.resetPosition();
+
+    //  Update all the enemies. Adapted from jlongster on github
+    for(var i=0; i<enemies.length; i++) {
+        enemies[i].pos[0] -= enemySpeed * dt;
+        enemies[i].sprite.update(dt);
+
+        // Remove if offscreen
+        if(enemies[i].pos[0] + enemies[i].sprite.size[0] < 0) {
+            enemies.splice(i, 1);
+            i--;
+        }
+    }
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +52,30 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-horn-girl.png';
+    this.x = 200;
+    this.y = 385;
+    this.moveY = 85;
+    this.moveX = 100;
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+
 // Place the player object in a variable called player
+var player = new Player();
+var enemy = new Enemy();
 
-
+enemy.render();
+player.render();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
